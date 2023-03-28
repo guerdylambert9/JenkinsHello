187bc645b0d305594063b5b05b2cec4b529aa48a
Guerdy Lambert <guerdylambert9@gmail.com>
2023-03-28 15:04:28 -0400

    Initial commit

---
+++ JenkinsHello/src/main/java/com/perscholas/jenkins_hello/HelloJenkins.java
@@ -0,0 +1,22 @@
+package com.perscholas.jenkins_hello;
+
+
+public class HelloJenkins {
+
+	//public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		public static void main(String[] args) {
+			System.out.println(helloJenkins());
+			System.out.println(helloJenkinsAgain());
+		}
+		
+		public static String helloJenkins() {
+			return "Hello Jenkins!";
+		}
+		
+		public static String helloJenkinsAgain() {
+			return "Hello Jenkins again!!!";
+		}
+
+}
